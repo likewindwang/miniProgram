e9868e80af2cb9732c3f0fb959de5632c4996aef
likewindwang <ityanqing@163.com>
2018-08-24 15:39:48 +0800

    search

--- pages/home/home.js
+++ pages/home/home.js
@@ -11,7 +11,29 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    //   wx.getSetting({
+    //       success: res => {
+    //           if (res.authSetting['scope.userInfo']) {
+    //               // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+    //               wx.getUserInfo({
+    //                   success: res => {
+
+    //                       // 可以将 res 发送给后台解码出 unionId
+    //                       this.globalData.userInfo = res.userInfo
+    //                       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //                       // 所以此处加入 callback 以防止这种情况
+    //                       if (this.userInfoReadyCallback) {
+    //                           this.userInfoReadyCallback(res)
+    //                       }
+    //                       console.log(res)
+    //                   },
+    //                   fail: function () {
+    //                       console.log('1')
+    //                   }
+    //               })
+    //           }
+    //       }
+    //   })
   },
     toHot(e){
         console.log(e)
@@ -25,6 +47,7 @@ Page({
             url: '../praise/praise?url=' + e.currentTarget.dataset.url,
         })
     },
+    // 获取用户信息
 
   /**
    * 生命周期函数--监听页面初次渲染完成
--- pages/search/search.js
+++ pages/search/search.js
@@ -4,16 +4,45 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+      items: [
+          { name: 'names', value: '按人名搜索', checked: 'true'},
+          { name: 'tags', value: '按类型搜索'},
+      ],
+      keyWay: 'names',
+      keyWords: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
-  },
 
+  },
+  radioChange: function (e) {
+      console.log('radio发生change事件，携带value值为：', e.detail.value)
+      this.setData({
+          keyWay: e.detail.value
+      })
+  },
+    getValue(e){
+        console.log(e.detail.value)
+        this.setData({
+            keyWords: e.detail.value
+        })
+    },
+    toD(){
+        if (this.data.keyWords!= ''){
+            wx.navigateTo({
+                url: '../searchD/searchD?keyWay=' + this.data.keyWay + '&keyWords=' + this.data.keyWords,
+            })
+        }else {
+            wx.showToast({
+                title: '搜索内容不能为空',
+                icon: 'none',
+                duration: 2500
+            })
+        }
+    },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
